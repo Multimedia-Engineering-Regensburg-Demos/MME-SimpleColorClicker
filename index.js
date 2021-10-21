3a92a62fc2bb424ec00d5b59fb88f32334b642e1
Alexander Bazo <alexanderbazo@googlemail.com>
2021-10-21 15:24:55 +0200

    Use open to open client in browser after server is started

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from "express";
+import open from "open";
 
 const HTTP_PORT = 8080;
 
 var app;
 
 function init() {
-  app = express();
-  app.use(express.static("app"));
-  app.listen(HTTP_PORT, function() {
-    console.log("Listening on Port " + HTTP_PORT);
-  });
+    app = express();
+    app.use("/", express.static("app"));
+    app.listen(HTTP_PORT, function() {
+        console.log("Server started. Opening application in browser ... [Press CTRL + C to stop server]");
+        open("http://localhost:8080");
+    });
 }
 
 init();
